697573e8acc6244b1b2831fb65440f575343994c
Lastochka1337 <shurigin.igor12@gmail.com>
2024-08-18 22:14:16 +0300

    Added migration file

--- products/admin.py
+++ products/admin.py
@@ -4,5 +4,18 @@ from products.models import Product, Order, OrderProduct
 
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(Order)
 admin.site.register(OrderProduct)
+
+class OrderProductInline(admin.TabularInline):
+    model = OrderProduct
+    extra = 1
+
+    fields = ('product', 'quantity',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderProductInline]
+
+    fields = ('user',)
+    readonly_fields = ('total_price',)
--- products/models.py
+++ products/models.py
@@ -1,10 +1,13 @@
-from django.db import models
+from django.db import models, transaction
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, null=True)
     is_18_plus = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -15,15 +18,24 @@ class Product(models.Model):
 
 class Order(models.Model):
     user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
-    total_price = models.FloatField()
+    total_price = models.FloatField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    products = models.ManyToManyField(Product)
+    products = models.ManyToManyField(Product, through="OrderProduct")
 
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
-    price = models.FloatField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    quantity = models.DecimalField(max_digits=10, decimal_places=3)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+@receiver(post_save, sender=Order)
+def update_order_total_price(sender, instance, **kwargs):
+    with transaction.atomic():
+        order = instance.order
+        order.total_price = sum([op.price for op in order.orderproduct_set.all()])
+        order.save()
+
+@receiver(pre_save, sender=OrderProduct)
+def update_order_product_price(sender, instance, **kwargs):
+    instance.price = instance.price * instance.quantity
